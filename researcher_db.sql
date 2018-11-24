8bc3766a81ae2f0a77ffb4e13c11630992672fb8
Tony Pan <tonypan2000@gmail.com>
2018-11-24 13:40:29 -0500

    database on MAMP

--- researcher_db.sql
+++ researcher_db.sql
@@ -1,11 +1,14 @@
-CREATE TABLE researcher
-(
-  id              INT unsigned NOT NULL AUTO_INCREMENT, # Unique ID for the record
-  name            VARCHAR(150) NOT NULL,                # Name of the researcher
-  title			      VARCHAR(150) NOT NULL,
-  email           VARCHAR(150) NOT NULL,
-  school          VARCHAR(150) NOT NULL,
-  department	    VARCHAR(150) NOT NULL,
-  password        VARCHAR(150) NOT NULL,
-  PRIMARY KEY     (id)                                  # Make the id the primary key
-);
+CREATE TABLE `haystack`.`researcher_data` (
+  `id` INT NOT NULL ,
+  `username` VARCHAR(50) NOT NULL ,
+  `name` VARCHAR(50) NOT NULL ,
+  `title` INT NOT NULL DEFAULT '1' ,
+  `email` VARCHAR(50) NOT NULL ,
+  `school` VARCHAR(50) NOT NULL ,
+  `department` VARCHAR(50) NOT NULL ,
+  `password` VARCHAR(50) NOT NULL ,
+  PRIMARY KEY (`id`),
+  UNIQUE (`username`),
+  UNIQUE (`email`)
+)
+ENGINE = InnoDB;
--- student_db.sql
+++ student_db.sql
@@ -1,9 +1,13 @@
-CREATE TABLE student
-(
-  id              INT unsigned NOT NULL AUTO_INCREMENT, # Unique ID for the record
-  name            VARCHAR(150) NOT NULL,                # Name of the student
-  email           VARCHAR(150) NOT NULL,
-  school          VARCHAR(150) NOT NULL,
-  password        VARCHAR(150) NOT NULL,
-  PRIMARY KEY     (id)                                  # Make the id the primary key
-);
+CREATE TABLE `haystack`.`student_data` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `username` VARCHAR(50) NOT NULL ,
+  `name` VARCHAR(50) NOT NULL ,
+  `email` VARCHAR(50) NOT NULL ,
+  `school` VARCHAR(50) NOT NULL ,
+  `password` VARCHAR(50) NOT NULL ,
+  PRIMARY KEY (`id`),
+  UNIQUE (`username`),
+  UNIQUE (`email`),
+  FULLTEXT (`name`)
+)
+ENGINE = InnoDB;
