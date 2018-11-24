afe65daa141595679348f2486221d3bfeabf5eff
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2018-11-24 17:09:05 -0500

    Basic Login works

---
+++ create_db_root.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 24, 2018 at 08:58 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `haystack`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
--- login.php
+++ login.php
@@ -2,31 +2,72 @@
 $message="";
 if(count($_POST)>0) {
 	$conn = mysqli_connect("localhost","haystack","Haystack1!","haystack");
-	$result = mysqli_query($conn,"SELECT * FROM users WHERE username='" . $_POST["userName"] . "' and password = '". $_POST["password"]."'");
+	$result = mysqli_query($conn,"SELECT * FROM users WHERE username='" . $_POST["inputEmail"] . "' and password = '". $_POST["inputPassword"]."'");
 	$count  = mysqli_num_rows($result);
 	if($count==0) {
-		$message = "Invalid Username or Password!";
+		$message = "Invalid email address or password.";
 	} else {
-		$message = "You are successfully authenticated!";
+		$message = "";
 	}
 }
 ?>
-<form name="frmUser" method="post" action="">
-	<div class="message"><?php if($message!="") { echo $message; } ?></div>
-		<table border="0" cellpadding="10" cellspacing="1" width="500" align="center" class="tblLogin">
-			<tr class="tableheader">
-			<td align="center" colspan="2">Enter Login Details</td>
-			</tr>
-			<tr class="tablerow">
-			<td>
-			<input type="text" name="userName" placeholder="User Name" class="login-input"></td>
-			</tr>
-			<tr class="tablerow">
-			<td>
-			<input type="password" name="password" placeholder="Password" class="login-input"></td>
-			</tr>
-			<tr class="tableheader">
-			<td align="center" colspan="2"><input type="submit" name="submit" value="Submit" class="btnSubmit"></td>
-			</tr>
-		</table>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-signin-client_id" content="796325909617-kh08o4a9o04gc5i1tkvmudgvheq1esj7.apps.googleusercontent.com">
+    <link rel="icon" href="icon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>For Students | Haystack</title>
+</head>
+<body class="bg-light">
+    <nav class="site-header sticky-top py-1">
+        <div class="container d-flex flex-column flex-md-row justify-content-between">
+            <a class="py-2 d-none d-md-inline-block text-light" href="index.html"><img src="logo.png" alt="HAYSTACK" width="180" height="26"></a>
+            <a class="py-2 d-none d-md-inline-block text-light" href="researcher.html">For Researchers</a>
+            <a class="py-2 d-none d-md-inline-block text-light" href="student.html">For Students</a>
+            <a class="py-2 d-none d-md-inline-block text-light" href="login.html">Login</a>
+            <a class="py-2 d-none d-md-inline-block btn btn-dark" href="signup.html">Sign Up</a>
+        </div>
+    </nav>
+    <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">Log In</h5>
+            <form class="form-signin" method="post" action="">
+              <div class="form-label-group">
+                <input type="email" name="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <label for="inputEmail">Email address</label>
+              </div>
+              <div class="form-label-group">
+                <input type="password" name="inputPassword" class="form-control" placeholder="Password" required>
+                <label for="inputPassword">Password</label>
+              </div>
+              <div class="text-danger"><?php if($message!="") { echo $message; } ?></div>
+              <div class="custom-control custom-checkbox mb-3">
+                <input type="checkbox" class="custom-control-input" name="rememberPass">
+                <label class="custom-control-label" for="customCheck1">Remember password</label>
+              </div>
+              <button name="submit" class="btn btn-lg btn-primary btn-block" type="submit">Log In</button>
+              <a class="btn btn-lg btn-dark btn-block" href="signup.html">Sign Up</a>
+              <hr class="my-4">
+              <button class="btn btn-lg btn-google btn-block" type="submit"><i class="fab fa-google mr-2"></i> Log in with Google</button>
+              <button class="btn btn-lg btn-linkedin btn-block" type="submit"><i class="fab fa-linkedin-f mr-2"></i> Log in with LinkedIn</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+</footer>
+</body>
+</html>
